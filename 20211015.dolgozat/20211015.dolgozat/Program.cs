26c0e595c64e0a5b84f6e7dc9a434f6ffa79b45b
kirchner-daniel <91724211+kirchner-daniel@users.noreply.github.com>
2021-10-15 10:34:39 +0200

    20211015.dolgozat

---
+++ 20211015.dolgozat/20211015.dolgozat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211015.dolgozat
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            //feladat4();
+            feladat2();
+        }
+        static void feladat4()
+        {
+            for (int i = -111; i >= 39; i=-7)
+            {
+                Console.Write("{0} ",i);
+            }
+            
+        }
+        static void feladat2()
+        {
+            Console.WriteLine("Kérek egy évszámot: ");
+            Console.ReadLine();
+            int i=
+            
+            if (true)
+            {
+
+            }
+        }
+    }
+}
